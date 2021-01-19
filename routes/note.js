74fbb689a06b5c924d8045c24f3dcb4711197b6c
yelim park <pyl9983@naver.com>
2021-01-19 14:23:04 +0900

    update category

--- routes/note.js
+++ routes/note.js
@@ -1,10 +1,18 @@
 var express = require('express');
 var router = express.Router();
 var Log = require('../models/log');
-var Comment = require('../models/comment');
+var Category = require('../models/category');
 
 router.get("/", async (req, res, next) => {
   const logs = await Log.find({});
+  // logs.map((elem) => {
+  //   const Cname = async() => {
+  //     await Category.findOne({ "_id":"600172ca5e37730de891b967"});
+  //     console.log(Cname.category);
+  //     elem.Cnum = Cname.category;
+  //     console.log(elem.Cnum);
+  //   }
+  // });
   res.json(logs);
 });
 
@@ -15,7 +23,7 @@ router.post("/", function(req,res,next){
   let log = new Log({
     TcTnum:"000",
     id:"abc",
-    Cnum:"000",
+    Cnum:req.body.Cnum,
     title:req.body.new_title,
     contents: req.body.new_text,
     logdate: now
