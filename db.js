569561272e98bec9bfe6651061228487e3fe16d9
Sa Eun Soo <tkdmstn0304@naver.com>
2021-01-30 14:30:50 +0900

    add dotenv library to db.js

--- db.js
+++ db.js
@@ -1,7 +1,8 @@
+const dotenv = require('dotenv');
 const mongoose = require('mongoose');
-require('dotenv').config;
 
 module.exports = () => {
+  dotenv.config()
   function connect() {
     mongoose.connect(process.env.DB_HOST, function(err) {
       if (err) {
