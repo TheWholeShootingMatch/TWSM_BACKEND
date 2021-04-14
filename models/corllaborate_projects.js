1ad15de009f14c4950512e5447f277214e7caf92
Jiseon-Lee <l990831@naver.com>
2021-04-14 12:54:29 +0900

    collaboration

--- models/corllaborate_projects.js
+++ models/corllaborate_projects.js
@@ -7,25 +7,100 @@ const corllaborateProjectSchema = new Schema({
       ref: "User",
       required: true,
     },
+    status:{
+      type: String,
+      required: true
+    },
     title:{
       type: String,
       required: true,
     },
-    contents:{
+    corporation_name:{
       type: String,
       required: true,
     },
-    status:{
-        type: Boolean,
-        required: true
-},
+    about_project:{
+      type: String,
+      required: true,
+    },
     location: {
-        type: Number,
+        type: String,
         required: true
     },
     Pdate: {
       type: Date,
       required: true,
+    },
+    model: {
+      type: Boolean,
+      required: true,
+    },
+    photographer: {
+      type: Boolean,
+      required: true,
+    },
+    gender: {
+      type: String,
+    },
+    age_min: {
+      type: Number,
+    },
+    age_max: {
+      type: Number,
+    },
+    height_min: {
+      type: Number,
+    },
+    height_max: {
+      type: Number,
+    },
+    weight_min: {
+      type: Number,
+    },
+    weight_max: {
+      type: Number,
+    },
+    busto_min: {
+      type: Number,
+    },
+    busto_max: {
+      type: Number,
+    },
+    quadril_min: {
+      type: Number,
+    },
+    quadril_max: {
+      type: Number,
+    },
+    cintura_min: {
+      type: Number,
+    },
+    cintura_max: {
+      type: Number,
+    },
+    ethnicity: {
+      type: String,
+    },
+    eye_color: {
+      type: String,
+    },
+    hair_color: {
+      type: String,
+    },
+    model_field: {
+      type: String,
+    },
+    model_detail: {
+      type: String,
+    },
+    photographer_field: {
+      type: String,
+    },
+    retouch: {
+      type: String,
+    },
+    photographer_detail: {
+      type: String,
     }
 });
 
--- routes/collaboration/collaboration.js
+++ routes/collaboration/collaboration.js
@@ -1,12 +1,72 @@
 var express = require('express');
 var router = express.Router();
-var collaboration = require("../../models/corllaborate_projects");
+var Collaboration = require("../../models/corllaborate_projects");
 
 router.get("/", async (req, res, next) => {
     let collaborations;
-    collaborations = await collaboration.find({});
+    collaborations = await Collaboration.find({});
     console.log(collaborations);
     res.json(collaborations);
 });
+
+router.post('/new', async(req, res, next) => {
+    console.log("add collaborate");
+    const update = new Collaboration({
+      id: req.session.user_Oid,
+      status : false,
+      title : req.body.title,
+      corporation_name : req.body.corporation_name,
+      about_project : req.body.about_project,
+      location : req.body.location,
+      Pdate : req.body.date,
+      model : req.body.Model,
+      photographer: req.body.Photographer,
+      gender: req.body.gender,
+      age_min: req.body.age_min,
+      age_max: req.body.age_max,
+      height_min: req.body.height_min,
+      height_max: req.body.height_max,
+      weight_min: req.body.weight_min,
+      weight_max: req.body.weight_max,
+      busto_min: req.body.busto_min,
+      busto_max: req.body.busto_max,
+      quadril_min: req.body.quadril_min,
+      quadril_max: req.body.quadril_max,
+      cintura_min: req.body.cintura_min,
+      cintura_max: req.body.cintura_max,
+      ethnicity: req.body.ethnicity,
+      eye_color: req.body.eye_color,
+      hair_color: req.body.hair_color,
+      model_field: req.body.model_field,
+      model_detail: req.body.model_detail,
+      photographer_field: req.body.photographer_field,
+      retouch: req.body.retouch,
+      photographer_detail: req.body.photographer_detail,
+    });
+
+    await update.save()
+    .then(() => res.send(true))
+    .catch(err => console.log(err));
+})
+
+router.post("/fetch", async (req, res, next) => {
+  if (req.body._id.match(/^[0-9a-fA-F]{24}$/)) {
+    const collaborate = await Collaboration.findOne({ _id:req.body._id }, (err) => {
+      if(err) {
+        console.log(err);
+      }
+    });
+    res.json(collaborate);
+  }
+});
+
+router.post("/delete", async (req, res, next) => {
+  console.log("delete");
+  Collaboration.deleteOne({_id:req.body._id}, function(err, slides) {
+    if(err) console.log(err);
+  });
+  res.send(true);
+});
+
 
 module.exports = router;
