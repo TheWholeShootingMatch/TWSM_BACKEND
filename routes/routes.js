39f604ea150476435ac422ca04bba27f4abf0449
Sa Eun Soo <tkdmstn0304@naver.com>
2021-05-31 22:25:42 +0900

    remove tct page

--- routes/routes.js
+++ routes/routes.js
@@ -8,14 +8,14 @@ var photographer = require("./photographer");
 var requestProject = require("./myPage/RequestProject");
 var myPage = require("./myPage/myPage");
 var notification = require("./myPage/notification");
-var whiteboard = require("./whiteboard");
+// var whiteboard = require("./whiteboard");
 var collaborations = require("./collaboration");
-var tct = require("./tct/tct");
-var tctmember = require("./tct/tctmember");
-var tctversion = require("./tct/tctversion");
+// var tct = require("./tct/tct");
+// var tctmember = require("./tct/tctmember");
+// var tctversion = require("./tct/tctversion");
 var bookmark = require("./myPage/bookmark");
 
-module.exports = function(app){
+module.exports = function (app) {
     app.use("/api/index", index);
     app.use("/api/users", users);
     app.use("/api/note", note);
@@ -25,21 +25,21 @@ module.exports = function(app){
     app.use("/api/photographer", photographer);
     app.use("/api/project", requestProject);
     app.use("/api/mypage", myPage);
-    app.use("/api/tct", tct);
-    app.use("/api/tctmember", tctmember);
+    // app.use("/api/tct", tct);
+    // app.use("/api/tctmember", tctmember);
     app.use("/api/notification", notification);
-    app.use("/api/whiteboard", whiteboard);
+    // app.use("/api/whiteboard", whiteboard);
     app.use("/api/collaboration", collaborations);
-    app.use("/api/tctversion", tctversion);
+    // app.use("/api/tctversion", tctversion);
     app.use("/api/bookmark", bookmark);
 
-    app.use(function(err, req, res, next) {
+    app.use(function (err, req, res, next) {
         // set locals, only providing error in development
         res.locals.message = err.message;
-        res.locals.error = req.app.get('env') === 'development' ? err : {};
+        res.locals.error = req.app.get("env") === "development" ? err : {};
 
         // render the error page
         res.status(err.status || 500);
-        res.render('error');
+        res.render("error");
     });
-}
+};
