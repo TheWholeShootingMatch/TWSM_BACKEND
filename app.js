28ef619ccd9c7577a7faef8e7678ea78198b5e7e
Sa Eun Soo <tkdmstn0304@naver.com>
2021-05-31 12:12:37 +0900

    prettier app

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var session = require('express-session');
-var FileStore = require('session-file-store')(session);
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var session = require("express-session");
+var FileStore = require("session-file-store")(session);
 
 var sessionMiddleware = session({
-  secret: 'keyboard cat',
-  resave: false,
-  saveUninitialized: true,
-  store: new FileStore() 
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: true,
+    store: new FileStore()
 });
 
 // var MongoStore = require('connect-mongo')(session);   //mongodb에 session저장 시 npm install connect-mongo
-const cors = require('cors');
-const db = require('./db');
+const cors = require("cors");
+const db = require("./db");
 
 //router
 var routes = require("./routes/routes");
@@ -23,30 +23,33 @@ var routes = require("./routes/routes");
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 
 //module setup
-app.use(logger('dev'));
-app.use(express.json({
-  limit: "50mb"
-}));
-app.use(express.urlencoded({
-  limit: "50mb",
-  extended: false
-}));
+app.use(logger("dev"));
+app.use(
+    express.json({
+        limit: "50mb"
+    })
+);
+app.use(
+    express.urlencoded({
+        limit: "50mb",
+        extended: false
+    })
+);
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(sessionMiddleware);
 app.use(cors());
-
 
 db();
 routes(app);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+    next(createError(404));
 });
 
 module.exports = { app, sessionMiddleware };
